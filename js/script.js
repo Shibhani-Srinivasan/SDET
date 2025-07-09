dc7fc7623a9699b5ed8f5b08ab1e390bb1bdbe57
Shibhani-Srinivasan <101919792+Shibhani-Srinivasan@users.noreply.github.com>
2025-07-09 18:11:57 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,5 @@
 /* ============================== typing animation ============================ */
-var typed = new Typed(".typing", {
-    strings: ["", "Quality Manager"],//, "Agile Project Manager"],
-    typeSpeed: 100,
-    BackSpeed: 60,
-    //loop: true
-})
+
 /* ============================== Aside ============================ */
 const nav = document.querySelector(".nav"),
     navList = nav.querySelectorAll("li"),
