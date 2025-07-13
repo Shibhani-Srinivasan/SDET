b496158307751d33e66ca3732bf9a8f99c190546
Shibhani-Srinivasan <101919792+Shibhani-Srinivasan@users.noreply.github.com>
2025-07-13 22:21:58 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* ============================== typing animation ============================ */
 var typed = new Typed(".typing", {
-    strings: ["","Program Manager – QA Automation", "Systems Thinker with a Creative Spine"],
+    strings: ["","Technical Program Manager – QA Automation", "Systems Thinker with a Creative Spine"],
     typeSpeed: 30,
     BackSpeed: 50,
     backDelay: 1000,   
